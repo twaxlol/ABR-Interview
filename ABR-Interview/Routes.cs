d5ca66942094d7f32cea74c23ca332b2dec7b07c
twaxlol <45589745+twaxlol@users.noreply.github.com>
2025-07-15 09:46:28 +0200

    Cleanup routes

---
+++ ABR-Interview/Routes.cs
@@ -0,0 +1,38 @@
+﻿namespace ABR_Interview;
+
+public static class Routes
+{
+    public static WebApplication AddRoutes(this WebApplication app)
+    {
+        app.MapPost("users/{userId}/guest-sessions", async (string userId, GuestSessionService service) =>
+        {
+            var result = await service.CreateSessionKey(userId);
+            if (result is null)
+            {
+                return Results.BadRequest("Session key already exists.");
+            }
+
+            return Results.Ok(result);
+        });
+
+        app.MapGet("/users/{userId}/guest-sessions/files", async (
+            string userId,
+            string sessionKey,
+            GuestSessionService guestSessionService,
+            FileService fileService) =>
+        {
+            var isValid = await guestSessionService.ValidateSessionKey(sessionKey, userId);
+            if (!isValid)
+            {
+                return Results.BadRequest("Invalid session key.");
+            }
+
+            await guestSessionService.UseSessionKey(sessionKey);
+            var files = await fileService.GetAllGuestFiles(userId);
+
+            return Results.Ok(files);
+        });
+
+        return app;
+    }
+}
